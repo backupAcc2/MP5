3c61484e627667b9d72c0e26ff340fe143a295ef
Heath Gerrald <heathgerrald@gmail.com>
2018-10-29 20:30:10 -0400

    Finished with project

---
+++ testscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Test Script for MP5
+# Heath Gerrald
+
+./lab5 -u 0
+./lab5 -u 7
+./lab5 -r -w 6 -t 0 -v
+./lab5 -o -w 20 -t 50000
+./lab5 -e -w 20 -t 100000
+./lab5 -r -v -s 1 -w 17
+./lab5 -p -w 16 -t 50000
+./lab5 -o -t 200000
+./lab5 -v -s 1 -w 16
+./lab5 -r -t 50000 -v -s 23035
